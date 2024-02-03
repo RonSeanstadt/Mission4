07de0bd5d0dc095b2a9aa6659306af4793e31159
RonSeanstadt <sronstadt470@gmail.com>
2024-02-02 22:15:35 -0700

    Final commit before pulling

--- Program.cs
+++ Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToe
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            char[,] board =
+            {
+                { 'X', 'O', 'O' },
+                { 'O', 'X', 'O' },
+                { 'X', ' ', 'O' }
+            };
+
+            Board.Print(board);
+
+            Console.WriteLine(Board.CheckForWinner(board));
+        }
+    }
+}
